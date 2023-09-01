d13a568ae70ea7750762770abdf82396c5ce60d8
das972811 <salasdiego54@gmail.com>
2023-08-31 19:42:12 -0500

    Added the ability to register coach

--- findACoach/src/pages/coaches/CoachesList.vue
+++ findACoach/src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
         <base-card>
             <div class="controls">
                 <base-button mode="outline">Refresh</base-button>
-                <base-button to="/register" link>Register as Coach</base-button>
+                <base-button v-if="!isCoach" to="/register" link>Register as Coach</base-button>
             </div>
             <ul v-if="hasCoaches">
                 <coach-item
@@ -43,6 +43,9 @@
             };
         },
         computed: {
+            isCoach() {
+                return this.$store.getters['coaches/isCoach'];
+            },
             filteredCoaches() {
                 const coaches = this.$store.getters['coaches/coaches'];
                 return coaches.filter(coach => {
